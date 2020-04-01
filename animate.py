c45f0af784510e0817f5424ab7dc5ebad9ac5b76
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-04-01 14:46:51 +0900

    Update animate.py

--- animate.py
+++ animate.py
@@ -2,10 +2,14 @@ from PIL import Image
 
 
 class gif:
-    def __init__(self, fps):
+    def __init__(self, name, fps, optimize=False, loop=0):
+        self.name = name
+        
         self.pictures = []
         self.frame = 0
         self.fps = fps
+        self.optimize = optimize
+        self.loop = loop
     
     def add_picture(self, picture):
         if type(picture) == list():
@@ -17,14 +21,7 @@ class gif:
         self.frame = len(self.pictures)
         
     def extract(self):
-        if self.frame <= 0:
-            raise Exception("blank frame.")
-        
-    
-
-
-
-
-if __name__ == "__main__":
-    gif = gif(15)
-    gif.extract()
+        if self.frame <= 1:
+            raise Exception("frame is less than 2.")
+        self.pictures[0].save(f'data/{self.name}.gif',
+               save_all=True, append_images=self.pictures[1:], optimize=self.optimize, duration=1000/self.fps, loop=self.loop)
